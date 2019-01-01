36e1b6fd41c940d22efd7fa38e90d78f85cd0f18
Tianjiao <wangtianjiao.wang959@gmail.com>
2019-01-01 14:16:10 +0800

    Remove SHA1

--- de4dot.cui/Program.cs
+++ de4dot.cui/Program.cs
@@ -76,7 +76,6 @@ namespace de4dot.cui {
 				new de4dot.code.deobfuscators.Unknown.DeobfuscatorInfo(),
 				new de4dot.code.deobfuscators.CodeFort.DeobfuscatorInfo(),
 				new de4dot.code.deobfuscators.CodeVeil.DeobfuscatorInfo(),
-				new de4dot.code.deobfuscators.DeepSea.DeobfuscatorInfo(),
 				new de4dot.code.deobfuscators.Dotfuscator.DeobfuscatorInfo(),
 				new de4dot.code.deobfuscators.Eazfuscator_NET.DeobfuscatorInfo(),
 				new de4dot.code.deobfuscators.Goliath_NET.DeobfuscatorInfo(),
